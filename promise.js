18ecaac2113b82fbb9b02c4933957a4cba64d5ad
moch agus rahmanto <agusrahmanto@mochs-MacBook-Pro.local>
2022-06-12 18:45:38 +0700

    tugas

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const teater1 = await promiseTheaterIXX();
+  const teater2 = await promiseTheaterVGC();
+
+  const kumpulanData = [...teater1, ...teater2];
+  const filterData = kumpulanData.filter((e) => e.hasil == emosi);
+  return filterData.length;
+};
 
 module.exports = {
   promiseOutput,
